a541ae679824972968004870fd00a5cea7efed18
Bao <bao9600455@yahoo.com.vn>
2024-06-28 14:15:21 -0400

    a

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,7 @@
 import HomePage from "../pages/HomePage/HomePage";
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 import Verify from "../pages/Verify/Verify";
-import HomePage from "../pages/HomePage/HomePage";
 import LocalBusinessPage from "../pages/LocalBusinessPage/LocalBusinessPage";
-import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 
 const routes = [
   {
