ffc24aa483aa45e045e1d5172cb72a91ddb91807
santiagobetancs <160289567+santiagobetancs@users.noreply.github.com>
2024-06-28 13:36:16 -0400

    added link to header categories

--- src/components/CatDesk/CatDesk.jsx
+++ src/components/CatDesk/CatDesk.jsx
@@ -1,11 +1,14 @@
 import './CatDesk.scss'
+import { Link } from 'react-router-dom'
 
-export default function CatDesk({ text }) {
+export default function CatDesk({ text, link }) {
     return (
         <>
-            <h4 className='catdesk'>
-                {text}
-            </h4>
+            <Link className='catdesk__link' to={link}>
+                <h4 className='catdesk'>
+                    {text}
+                </h4>
+            </Link>
         </>
     )
 }
