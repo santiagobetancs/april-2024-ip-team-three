4f2fd14d7c7dbf3d775308d3b2252d4f257f0c8a
santiagobetancs <160289567+santiagobetancs@users.noreply.github.com>
2024-06-28 14:28:47 -0400

    develop

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,10 +2,6 @@ import HomePage from "../pages/HomePage/HomePage";
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 import Verify from "../pages/Verify/Verify";
 import LocalBusinessPage from "../pages/LocalBusinessPage/LocalBusinessPage";
-<<<<<<< HEAD
-
-=======
->>>>>>> 41c545802206ca1760e697a856fbe57be4a864b8
 
 const routes = [
   {
