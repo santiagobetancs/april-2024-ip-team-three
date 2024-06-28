74d4c04eba51dd568d62eace6bef80badd332113
Jenna Knechtel <jmknec@gmail.com>
2024-06-28 13:05:12 -0400

    add icons to header scroll menu

--- src/categories/categories.js
+++ src/categories/categories.js
@@ -1,15 +1,59 @@
-import tag from '../assets/icons/tag.png'
-import smallBusiness from '../assets/icons/small-business.png'
+import tag from "../assets/icons/tag.png";
+import smallBusiness from "../assets/icons/small-business.png";
+import car from "../assets/icons/icon_car_.svg";
+import house from "../assets/icons/icon_house_.svg";
+import jobs from "../assets/icons/icon_work_.svg";
+import services from "../assets/icons/Services.svg";
+import pets from "../assets/icons/pets.svg";
+import community from "../assets/icons/people-community.svg";
+import vacation from "../assets/icons/vacation.svg";
 
 const categories = [
-    {
-        img: smallBusiness,
-        text: "Small Businesses"
-    },
-    {
-        img: tag,
-        text: "Buy & Sell"
-    }
-]
+  {
+    img: smallBusiness,
+    text: "Small Businesses",
+    link: "/small-business",
+  },
+  {
+    img: tag,
+    text: "Buy & Sell",
+    link: "/buy-sell",
+  },
+  {
+    img: car,
+    text: "Cars & Vehicles",
+    link: "/auto",
+  },
+  {
+    img: house,
+    text: "Real Estate",
+    link: "/real-estate",
+  },
+  {
+    img: jobs,
+    text: "Jobs",
+    link: "/jobs",
+  },
+  {
+    img: services,
+    text: "Services",
+    link: "/services",
+  },
+  {
+    img: pets,
+    text: "Pets",
+    link: "/pets",
+  },
+  {
+    img: community,
+    text: "Community",
+    link: "/community",
+  },
+  {
+    img: vacation,
+    text: "Vacation Rentals",
+    link: "/vacation",
+  },
+];
 
-export default categories
+export default categories;
